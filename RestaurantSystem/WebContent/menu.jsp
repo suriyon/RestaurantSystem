b3e372f165e763a8e4821fb2230a8e2a75bd196f
Suriyon <Suriyon@Suriyon-PC>
2017-01-16 12:07:41 +0700

    Third Commit

--- RestaurantSystem/WebContent/menu.jsp
+++ RestaurantSystem/WebContent/menu.jsp
@@ -1,6 +1,6 @@
 <%@ page language="java" contentType="text/html; charset=UTF-8"
     pageEncoding="UTF-8"%>
-<a href="#">หน้าหลัก</a> |
+<a href="EmployeeServlet?action=selectall">หน้าหลัก</a> |
 <a href="#">ค้นหา</a> |
 <a href="insert.jsp">เพิ่มข้อมูล</a> |
 <a href="#">แก้ไขข้อมูล</a> |
--- RestaurantSystem/src/controller/EmployeeServlet.java
+++ RestaurantSystem/src/controller/EmployeeServlet.java
@@ -1,11 +1,17 @@
 package controller;
 
 import java.io.IOException;
+import java.util.List;
+
+import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+
+import dao.EmployeeDAO;
+import model.Employee;
 
 /**
  * Servlet implementation class EmployeeServlet
@@ -45,7 +51,39 @@ public class EmployeeServlet extends HttpServlet {
 			int position = Integer.parseInt(request.getParameter("position"));
 			String ssalary = request.getParameter("salary");
 			
+			Employee employee = new Employee();
+			employee.setId(id);
+			employee.setName(name);
+			employee.setPosition(position);
+			if(ssalary.equals("")){
+				
+			}else{
+				int salary = Integer.parseInt(ssalary);
+				employee.setSalary(salary);
+			}
 			
+			EmployeeDAO dao = new EmployeeDAO();
+			boolean result = dao.invalidId(id);
+			if(result){
+				RequestDispatcher rd = request.getRequestDispatcher("insert.jsp");
+				request.setAttribute("employee", employee);
+				request.setAttribute("message", "รหัสซ้ำ กรอกรหัสใหม่");
+				rd.forward(request, response);
+			}else{
+				result = dao.insert(employee);
+				if(result){
+					List<Employee> employees = dao.selectAll();
+					RequestDispatcher rd = request.getRequestDispatcher("home.jsp");
+					request.setAttribute("employees", employees);
+					rd.forward(request, response);
+				}
+			}
+		}else if(action.equals("selectall")){
+			EmployeeDAO dao = new EmployeeDAO();
+			List<Employee> employees = dao.selectAll();
+			RequestDispatcher rd = request.getRequestDispatcher("home.jsp");
+			request.setAttribute("employees", employees);
+			rd.forward(request, response);
 		}
 	}
 
